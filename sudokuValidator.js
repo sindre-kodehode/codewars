08eb14dcd2c665c91a40bf359c7c46732e8b4167
sindre-kodehode <sindre.kodehode@gmail.com>
2023-01-14 17:24:24 +0100

    add codewars files

---
+++ sudokuValidator.js
@@ -0,0 +1,84 @@
+#! /usr/bin/env node
+"use strict"
+import assert from "node:assert"
+
+//******************************************************************************
+// Sudoku Background                                                           *
+//                                                                             *
+// Sudoku is a game played on a 9x9 grid. The goal of the game is to fill      *
+// all cells of the grid with digits from 1 to 9, so that each column,         *
+// each row, and each of the nine 3x3 sub-grids (also known as blocks)         *
+// contain all of the digits from 1 to 9.                                      *
+//                                                                             *
+// (More info at: http://en.wikipedia.org/wiki/Sudoku)                         *
+//                                                                             *
+// Sudoku Solution Validator                                                   *
+//                                                                             *
+// Write a function validSolution/ValidateSolution/valid_solution() that       *
+// accepts a 2D array representing a Sudoku board, and returns true if it      *
+// is a valid solution, or false otherwise. The cells of the sudoku board      *
+// may also contain 0's, which will represent empty cells. Boards containing   *
+// one or more zeroes are considered to be invalid solutions.                  *
+//                                                                             *
+// The board is always 9 cells by 9 cells, and every cell only contains        *
+// integers from 0 to 9.                                                       *
+//******************************************************************************
+
+const validSolution = [
+  rows => rows
+    .flatMap((row, i) => {
+      const y = (i % 3) * 3;
+      const x = i - (i % 3);
+
+      const column = row.map((_, x) => rows[x][i]);
+      const block = [
+        ...rows[y + 0].slice(x, x + 3),
+        ...rows[y + 1].slice(x, x + 3),
+        ...rows[y + 2].slice(x, x + 3),
+      ];
+
+      return [row, column, block];
+    })
+    .every(e => new Set(e).size === 9),
+];
+
+
+
+
+//******************************************************************************
+//  Tests                                                                      *
+//******************************************************************************
+
+const tests = [
+  [[
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ], true],
+
+  [[
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ], false],
+];
+
+console.log("--- Running tests", "-".repeat(62));
+
+for (const func of validSolution)
+  for (const [input, expected] of tests)
+    assert.deepEqual(func(input), expected);
+
+console.log("--- Tests OK", "-".repeat(67));
